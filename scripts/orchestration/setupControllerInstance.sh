28564d5a36777ba3bae4b139eb1cb436bd2fa57d
424D57 <bryan_webster@trendmicro.com>
2017-01-23 09:15:42 -0600

    Set owner for pulls from github

--- scripts/orchestration/setupControllerInstance.sh
+++ scripts/orchestration/setupControllerInstance.sh
@@ -13,6 +13,8 @@ sudo yum -y install libunwind libicu
 
 cd /home/ec2-user/
 git clone https://github.com/424D57/tmThreatChallenge.git
+chown -R ec2-user:ec2-user tmThreatChallenge
+chown -R ec2-user:2c2-user variables
 ##todo: aws create-key-pair threatChallengeKeyPair for DSM and team instances
 ##todo: setup bashrc
 
--- scripts/orchestration/setupCtfInstance.sh
+++ scripts/orchestration/setupCtfInstance.sh
@@ -4,5 +4,6 @@ cd /home/ubuntu/
 apt-get update
 apt-get install git
 git clone https://github.com/wisco24/fbctf.git fbctf
+chown -R ubuntu:ubuntu fbctf
 cd fbctf
 ./extra/provision.sh -m dev -s $PWD
