2d76e1070aeea3024692d8489d5f04ca7a1569bb
424D57 <bryan_webster@trendmicro.com>
2017-01-26 17:14:23 -0600

    Remove hard code stack name

--- scripts/threatChallengeLaunch/launchMtDSM.sh
+++ scripts/threatChallengeLaunch/launchMtDSM.sh
@@ -8,13 +8,11 @@ vpc=${4}
 dsmSubnet=${5}
 dbSubnet1=${6}
 dbSubnet2=${7}
-eventName=${8}
-stackname="tmtcDsStack"
+stackname=${8}
+eventName=${9}
 
-if [ -n ${8} ]
-then
-    stackname=${8}
-fi
+
+
 
 
 aws cloudformation create-stack --disable-rollback \
