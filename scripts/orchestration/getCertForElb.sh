c1744f2f9d30aa60179fd359c97cfa04847ccecf
424D57 <bryan_webster@trendmicro.com>
2017-01-27 18:35:29 -0600

    Allow multiple letsencrypt certs in same aws account

--- scripts/orchestration/getCertForElb.sh
+++ scripts/orchestration/getCertForElb.sh
@@ -4,6 +4,6 @@ curl -O https://dl.eff.org/certbot-auto
 chmod a+x certbot-auto
 ./certbot-auto --debug certonly --webroot -w /usr/share/nginx/html/ -d ${dsmFqdn} --non-interactive --agree-tos --email event@trenddemos.com
 sudo chown -R ec2-user:ec2-user /etc/letsencrypt
-uploadResponse=$(aws iam upload-server-certificate --server-certificate-name event.trenddemos.com --certificate-body file:///etc/letsencrypt/archive/${dsmFqdn}/cert1.pem --private-key file:///etc/letsencrypt/archive/${dsmFqdn}/privkey1.pem)
+uploadResponse=$(aws iam upload-server-certificate --server-certificate-name ${dsmFqdn} --certificate-body file:///etc/letsencrypt/archive/${dsmFqdn}/cert1.pem --private-key file:///etc/letsencrypt/archive/${dsmFqdn}/privkey1.pem)
 arn=$(echo $uploadResponse | jq -r .ServerCertificateMetadata.Arn)
 echo $arn > /home/ec2-user/variables/certArn
