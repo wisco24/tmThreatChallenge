c817a27b1c945a1d90f13436da91b63c1a8b878f
424D57 <bryan_webster@trendmicro.com>
2017-01-28 15:29:04 -0600

    Add letencrypt dsm cert root to ctrl trusted store

--- scripts/orchestration/getCertForElb.sh
+++ scripts/orchestration/getCertForElb.sh
@@ -8,3 +8,5 @@ aws iam delete-server-certificate --server-certificate-name ${dsmFqdn}
 uploadResponse=$(aws iam upload-server-certificate --server-certificate-name ${dsmFqdn} --certificate-body file:///etc/letsencrypt/archive/${dsmFqdn}/cert1.pem --private-key file:///etc/letsencrypt/archive/${dsmFqdn}/privkey1.pem)
 arn=$(echo $uploadResponse | jq -r .ServerCertificateMetadata.Arn)
 echo $arn > /home/ec2-user/variables/certArn
+sudo cp /etc/letsencrypt/archive/${dsmFqdn}/fullchain1.pem /etc/pki/ca-trust/source/anchors/
+sudo update-ca-trust
