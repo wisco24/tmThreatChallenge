91c899c824f26ead51f0c28fe7cc502a33fbf0d5
424D57 <bryan_webster@trendmicro.com>
2017-01-28 13:17:33 -0600

    Skip delete if record doesn't exist

--- scripts/orchestration/delDsmRoute53.sh
+++ scripts/orchestration/delDsmRoute53.sh
@@ -4,6 +4,10 @@ dnsname=${1}
 recordSetName="'"${dnsname}."'"
 
 dsmRecordDnsName=$(aws route53 list-resource-record-sets --hosted-zone-id Z54BUX0B2EC7C --query "ResourceRecordSets[?Name == ${recordSetName} ]" | jq -r .[].AliasTarget.DNSName)
+if [[ ${dsmRecordDnsName} == "" ]]
+then
+    exit 0
+fi
 hostedZoneId=$(aws route53 list-resource-record-sets --hosted-zone-id Z54BUX0B2EC7C --query "ResourceRecordSets[?Name == ${recordSetName} ]" | jq -r .[].AliasTarget.HostedZoneId)
 
 aws route53 change-resource-record-sets --cli-input-json '{
