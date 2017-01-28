b6af14a98ec129314db5af81747b30c11c67f52e
424D57 <bryan_webster@trendmicro.com>
2017-01-28 09:27:51 -0600

    Delete cert by name in case event name was previously used

--- scripts/orchestration/getCertForElb.sh
+++ scripts/orchestration/getCertForElb.sh
@@ -4,6 +4,7 @@ curl -O https://dl.eff.org/certbot-auto
 chmod a+x certbot-auto
 ./certbot-auto --debug certonly --webroot -w /usr/share/nginx/html/ -d ${dsmFqdn} --non-interactive --agree-tos --email event@trenddemos.com
 sudo chown -R ec2-user:ec2-user /etc/letsencrypt
+aws iam delete-server-certificate --server-certificate-name ${dsmFqdn}
 uploadResponse=$(aws iam upload-server-certificate --server-certificate-name ${dsmFqdn} --certificate-body file:///etc/letsencrypt/archive/${dsmFqdn}/cert1.pem --private-key file:///etc/letsencrypt/archive/${dsmFqdn}/privkey1.pem)
 arn=$(echo $uploadResponse | jq -r .ServerCertificateMetadata.Arn)
 echo $arn > /home/ec2-user/variables/certArn
