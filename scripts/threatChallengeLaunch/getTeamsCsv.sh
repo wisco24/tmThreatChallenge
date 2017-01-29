c13a197adbdbb679efdd49029456bd1fd9903107
424D57 <bryan_webster@trendmicro.com>
2017-01-29 15:32:23 -0600

    Replace special character stripped by intellij

--- scripts/threatChallengeLaunch/getTeamsCsv.sh
+++ scripts/threatChallengeLaunch/getTeamsCsv.sh
@@ -5,7 +5,6 @@ sanitizedFilename=sanitizedTeamCreds
 
 aws s3 cp s3://${bucket}/Passwords.csv ./ > /dev/null
 tail -n +2 Passwords.csv | awk -F "," '{print $1 " " $2}' > ${filename}
-sed -e "s/
-//" ${filename} > ${sanitizedFilename}
+sed -e "s///" ${filename} > ${sanitizedFilename}
 rm ${filename}
 echo ${sanitizedFilename}
