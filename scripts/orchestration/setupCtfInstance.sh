153b2276af4b9e4fbffbc2ce9efd96615b928b8f
424D57 <bryan_webster@trendmicro.com>
2017-01-28 20:47:07 -0600

    Set real cert and admin password for ctf

--- scripts/orchestration/setupCtfInstance.sh
+++ scripts/orchestration/setupCtfInstance.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+dsmT0Password=$(cat /home/ec2-user/variables/t0AdminPassword)
+baseDomain=$(cat /home/ec2-user/variables/baseDomain)
+eventName=$(cat /home/ec2-user/variables/eventName)
+
 cd /home/ubuntu/
 apt-get update
 apt-get -y install git
@@ -7,4 +11,6 @@ git clone https://github.com/wisco24/fbctf.git fbctf
 chown -R ubuntu:ubuntu fbctf
 cd fbctf
 export HOME=/root
-./extra/provision.sh -m prod -c self -s $PWD
+source ./extra/lib.sh
+set_password ${dsmT0Password} ctf ctf fbctf $PWD
+./extra/provision.sh -m prod -c certbot -D ctf.${eventName}.${baseDomain} -e admin@${baseDomain} -s $PWD
