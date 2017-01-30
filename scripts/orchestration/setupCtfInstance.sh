19860502205fd4cac8b082bbae9055b6df830ddd
424D57 <bryan_webster@trendmicro.com>
2017-01-29 18:20:34 -0600

    Install ctf before changing password

--- scripts/orchestration/setupCtfInstance.sh
+++ scripts/orchestration/setupCtfInstance.sh
@@ -11,6 +11,6 @@ git clone https://github.com/wisco24/fbctf.git fbctf
 chown -R ubuntu:ubuntu fbctf
 cd fbctf
 export HOME=/root
+./extra/provision.sh -m prod -c certbot -D ctf.${eventName}.${baseDomain} -e admin@${baseDomain} -s $PWD
 source ./extra/lib.sh
 set_password ${dsmT0Password} ctf ctf fbctf $PWD
-./extra/provision.sh -m prod -c certbot -D ctf.${eventName}.${baseDomain} -e admin@${baseDomain} -s $PWD
