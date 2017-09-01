3875035bf527ef7b89c1cbf53fea77069686fd0e
Wisco24 <eshulze@gmail.com>
2017-08-31 23:07:28 -0500

    Fixing Permission Issue

--- scripts/orchestration/setupCtfInstance.sh
+++ scripts/orchestration/setupCtfInstance.sh
@@ -4,13 +4,8 @@ dsmT0Password=$(cat /home/ubuntu/variables/t0AdminPassword)
 baseDomain=$(cat /home/ubuntu/variables/baseDomain)
 eventName=$(cat /home/ubuntu/variables/eventName)
 
-cd /home/ubuntu/
-apt-get update
-apt-get -y install git
-git clone https://github.com/wisco24/fbctf.git fbctf
 chown -R ubuntu:ubuntu fbctf
-cd fbctf
+cd /home/ubuntu/fbctf
 export HOME=/root
-./extra/provision.sh -m prod -c certbot -D ctf.${eventName}.${baseDomain} -e admin@${baseDomain} -s $PWD
-source ./extra/lib.sh
-set_password ${dsmT0Password} ctf ctf fbctf $PWD
+chmod +x extra/certupdate.sh
+sudo ./extra/certupdate.sh admin@${baseDomain} ctf.${eventName}.${baseDomain} ${dsmT0Password}
